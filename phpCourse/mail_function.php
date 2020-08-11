9830106e27c1e6ca1a308f0b0ec2c72886d32f04
Mahmoud Ragab <mahmoud.dahab99@gmail.com>
2020-08-11 18:16:26 +0200

    This is the error msg in the output.. can you solve it

--- phpCourse/mail_function.php
+++ phpCourse/mail_function.php
@@ -1,6 +1,16 @@
 <?php
-$to = "m_ragab333@yahoo.com";
-$subject = "A new message";
-$message = "test for the mail function using php";
-mail($to,$subject,$message);
+ini_set("SMTP","192.168.1.1");
+ini_set('sendmail_from', 'mahmoud.dahab99@gmail.com');
+$to      = 'm_ragab333@yahoo.com';
+$subject = 'A new message';
+$message = 'Hello Mohamed';
 
+mail($to, $subject, $message);
+/*
+
+ OUTPUT ==>
+
+    Warning: mail(): Failed to connect to mailserver at "192.168.1.1" port 25,
+ verify your "SMTP" and "smtp_port" setting in php.ini or use ini_set() in C:\xampp\htdocs\phpCourse\mail_function.php on line 8
+
+*/
